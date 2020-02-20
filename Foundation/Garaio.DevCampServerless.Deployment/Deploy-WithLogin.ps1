b83f64f7ce4c2d4328e056f9705c5dbbb804f216
Jonas Schmied <jonas.schmied@garaio.com>
2020-02-20 14:20:43 +0100

    Fixed some issues preventing deployment of functions

---
+++ Foundation/Garaio.DevCampServerless.Deployment/Deploy-WithLogin.ps1
@@ -0,0 +1,9 @@
+﻿Connect-AzAccount
+
+Get-AzSubscription
+
+$SubscriptionId = Read-Host -Prompt 'Enter id of subscription you want to use for deployment'
+
+Set-AzContext -SubscriptionId $SubscriptionId
+
+& ((Split-Path $MyInvocation.InvocationName) + "\Deploy-AzTemplate.ps1")
